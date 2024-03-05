f744229504d6bc10b090e43ceafc5df6d2ba5223
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-03-05 12:41:03 -0700

    fixed bug related to the required fields error message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 const submitButton = document.querySelector('#submit')
+const requiredInputs = document.querySelectorAll('input[required]');
+const formContainer = document.querySelector('.form-container');
 
 let passwordValue;
 let confirmPasswordValue;
@@ -14,11 +16,29 @@ confirmPassword.addEventListener('input', () => {
 })
 
 submitButton.onclick = function() {
-    if (passwordValue != confirmPasswordValue) {
-        event.preventDefault(); // Prevent form from submitting
-        document.getElementById('password-error').textContent = 'Passwords do not match.'; // Display error message in HTML
+    let isFormFilled = true;
+    requiredInputs.forEach(input => {
+        if (!input.value) {
+            isFormFilled = false;
+            const span = input.previousElementSibling;
+            if (!span.textContent.startsWith('*')) {
+                span.textContent = '*' + span.textContent;
+            }
+        }
+    });
+
+    if (!isFormFilled) {
+        event.preventDefault();
+        document.getElementById('field-error').textContent = 'Please fill out the required fields.';
     } else {
-        document.getElementById('password-error').textContent = ''; // Clear error message when passwords match
+        document.getElementById('field-error').textContent = '';
+    }
+
+    if (passwordValue != confirmPasswordValue) {
+        event.preventDefault();
+        document.getElementById('password-error').textContent = 'Passwords do not match.';
+    } else {
+        document.getElementById('password-error').textContent = '';
     }
 }
 
